1413ce2ef4f9605883430e253e5aba39baddc9aa
Yarushin Sergey <s_yarushin@gcore.lu>
2021-11-25 20:57:50 +0500

    added basic styles

--- gcore-test-product/src/app/main/main-routing.module.ts
+++ gcore-test-product/src/app/main/main-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { HelloWorldComponent } from "../hello-world/hello-world.component";
 
-const routes: Routes = [{ path: '', component: HelloWorldComponent },{path: 'test', component: MainComponent }];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'test', component: HelloWorldComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
